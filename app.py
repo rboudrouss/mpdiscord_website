4098b8b9247cdcdf6a359a833e28b83e8eaec18a
rboudrouss <40902229+rboudrouss@users.noreply.github.com>
2020-08-08 02:34:32 +0100

    must add acount management

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
-from json import loads
+from flask import Flask, render_template,request,redirect,url_for
+from json import loads,dumps
 app = Flask(__name__)
-IDUSER = "anonymous"
+IDUSER = ""
 
 @app.route('/')
 def home():
@@ -15,12 +15,45 @@ def on():
 def off():
     return render_template("useroff.html")
 
-@app.route('/login')
+@app.route('/admin')
+def admin():
+    return "admin"
+
+@app.route('/login', methods=['POST','GET'])
 def login():
+    global IDUSER
+    if request.method == "POST":
+        mail=request.form["mail"]
+        pwd=request.form["password"]
+        with open("./static/data/accounts.json",'r') as f:
+            accounts = loads(f.read())
+            if accounts.get(mail,False):
+                if accounts[mail]["password"] == pwd:
+                    IDUSER = accounts[mail]["id"]
+                    print("logged in as", IDUSER)
+                    return redirect(url_for('mymps'))
+                else:
+                    print("wrong password")
+            else:
+                print("wrong email")
     return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register',methods=["POST","GET"])
 def register():
+    global IDUSER
+    if request.method == "POST":
+        mail=request.form["mail"]
+        pwd=request.form["password"]
+        id_d = request.form["id"]
+        with open("./static/data/accounts.json",'r') as f:
+            accounts = loads(f.read())
+        accounts[mail]={
+            "id":id_d,
+            "password": pwd
+        }
+        with open("./static/data/accounts.json",'w') as f:
+            f.write(dumps(accounts,sort_keys=True, indent=4))
+        return redirect(url_for("home"))
     return render_template("register.html")
 
 @app.route('/u/<user>')
@@ -42,6 +75,25 @@ def userlist():
         users = loads(f.read())
         return render_template("userlist.html",users=users)
 
+@app.route('/mymps',methods=["POST","GET"])
+def mymps():
+    if not IDUSER:
+        with open("./static/data/users.json",'r') as f:
+            users = loads(f.read())
+            mps_statu = users[IDUSER]
+        if request.method == "POST":
+            with open("./static/data/users.json",'r') as f:
+                users = loads(f.read())
+            users[IDUSER] = "on" if users[IDUSER] == "off" else "off"
+            with open("./static/data/users.json",'w') as f:
+                f.write(dumps(users,sort_keys=True, indent=4))
+        return render_template("mymps.html", mp = mps_statu)
+    else:
+        return redirect(url_for("login"))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "404", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
